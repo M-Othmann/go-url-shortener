fa7c261bdd547c4bb846d80e72cf73943c83238e
Mohamed Othman <othmanelrabiey@gmail.com>
2026-02-27 08:14:33 +0200

    feat: init data store service

---
+++ store/store_service.go
@@ -0,0 +1,36 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type StorageService struct {
+	redisClient *redis.Client
+}
+
+var (
+	storeService = &StorageService{}
+	ctx          = context.Background()
+)
+
+const CacheDuration = 6 * time.Hour
+
+func InitializeStore() *StorageService {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Error init redis: %v", err))
+	}
+	fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
+	storeService.redisClient = redisClient
+	return storeService
+}
